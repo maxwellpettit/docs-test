8bb4824a8455cd80564e25a0c3535ca301af9d0c
Mark Michalek <mmichalek@jumpmind.com>
2020-06-23 14:17:26 -0400

    generate HTML document also

--- build-doc.sh
+++ build-doc.sh
@@ -40,13 +40,32 @@ echo $'\n' >>  out/JumpMind-Commerce-User-Manual.md
 cat "src/main/markdown/Appendix/Appendix.md" >> out/JumpMind-Commerce-User-Manual.md
 echo $'\n' >>  out/JumpMind-Commerce-User-Manual.md
 
-RESOURCE_SEARCH_PATH="src/main/markdown/Appendix/:src/main/markdown/Beyond POS:src/main/markdown/Backoffice:src/main/markdown/Tax/:src/main/markdown/Orders/:src/main/markdown/Training Mode/:src/main/markdown/Peripherals/:src/main/markdown/Coupons/:src/main/markdown/Sales/:src/main/markdown/UI & Screen Conventions/:src/main/markdown/User Management/:src/main/markdown/Daily Operations/:src/main/markdown/Item Inquiry/:src/main/markdown/Returns Management/:rc/main/markdown/Tendering/:src/main/markdown/Receipts/:src/main/markdown/Customers/:src/main/markdown/Promotions/"
+mkdir -p out/html/assets
 
+ASSET_LOCATIONS=("src/main/markdown/Appendix/" "src/main/markdown/Backoffice/" "src/main/markdown/Tax/" "src/main/markdown/Orders/" "src/main/markdown/Training Mode/" "src/main/markdown/Peripherals/" "src/main/markdown/Coupons/" "src/main/markdown/Sales/" "src/main/markdown/UI & Screen Conventions/" "src/main/markdown/User Management/" "src/main/markdown/Daily Operations/" "src/main/markdown/Returns Management/" "src/main/markdown/Tendering/" "src/main/markdown/Receipts/" "src/main/markdown/Customers/" "src/main/markdown/Promotions/" "src/main/markdown/Beyond POS/")
 
+RESOURCE_SEARCH_PATH=""
+
+for i in "${ASSET_LOCATIONS[@]}"
+do
+   : 
+   RESOURCE_SEARCH_PATH=$RESOURCE_SEARCH_PATH:"$i"
+   echo cp -R "\"$i\"" out/html/
+   cp -R "$i" out/html/
+done
+
+echo $RESOURCE_SEARCH_PATH
 
 # Convert to docx
 pandoc --resource-path "$RESOURCE_SEARCH_PATH" -s out/JumpMind-Commerce-User-Manual.md -o out/JumpMind-Commerce-User-Manual.docx
 
+cp src/main/markdown/htmlStyle.css out/html
+pandoc --css htmlStyle.css --resource-path "$RESOURCE_SEARCH_PATH" -s out/JumpMind-Commerce-User-Manual.md -o out/html/JumpMind-Commerce-User-Manual.html
+
+rm -rf out/html/*.md
+
 open out/JumpMind-Commerce-User-Manual.docx
+
+open out/html/JumpMind-Commerce-User-Manual.html
 
 echo "Generated out/JumpMind-Commerce-User-Manual.docx" 
